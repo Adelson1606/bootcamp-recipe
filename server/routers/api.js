d3f436fa3d57b5d498f0787b41af4f72fd8bbd09
Veronika Adelson <adelson1606@gmail.com>
2020-05-06 11:22:33 +0300

    data from API

--- server/routers/api.js
+++ server/routers/api.js
@@ -1,9 +1,17 @@
 const express = require('express')
-// const urllib = require('urllib')
+const urllib = require('urllib')
 const router = express.Router()
 
-router.get('/', function (request, response) {
-  response.send("hello")
+router.get('/sanity', function (request, response) {
+  response.send("OK")
+})
+
+router.get('/recipes/:ingredient', function (req, res) {
+  const ingredient = req.params.ingredient
+  urllib.request(`https://recipes-goodness.herokuapp.com/recipes/${ingredient}`, function (_err, data) {
+    console.log(data.toString())
+  }) 
+  res.send('take data')
 })
 
 module.exports = router
