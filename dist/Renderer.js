46a3cf1d56916a55269385932af0fa0c2e4208a8
Veronika Adelson <adelson1606@gmail.com>
2020-05-06 15:21:04 +0300

    toLowerCase and err of data added

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -2,10 +2,17 @@ class Renderer {
 
   render (data) {
     $('.results').empty()
-    const source = $('#template').html()
-    const template = Handlebars.compile(source)
-    const newHTML = template(data)
-    $('.results').append(newHTML)
+    if (data.allRecipes.length === 0) { 
+      $('.error').append(`
+        <div> wrong input! Try another request like: oil, cheese, tomato, etc.</div>
+        `) 
+    } else {
+      const source = $('#template').html()
+      const template = Handlebars.compile(source)
+      const newHTML = template(data)
+      $('.results').append(newHTML)
+    }
+  
   }
   
 }
--- dist/main.js
+++ dist/main.js
@@ -1,16 +1,14 @@
 const renderer = new Renderer()
 
 const show = function () {
-  const input = $('#input-ingredient').val()
+  const input = $('#input-ingredient').val().toLowerCase()
   $.get(`recipes/${input}`, function (allRecipes) {
-    renderer.render({ allRecipes })
-    $('.photo').on('click', function () {
-      console.log('clicked photo')
-      const firsIngr = $(this).closest(".recipe-container").find('li').first().text()
-      console.log(firsIngr)
-    })
+    renderer.render({ allRecipes })  
   })
-  
-
 }
 
+$('.results').on('click', '.photo', function () {
+  console.log('clicked photo')
+  const firsIngr = $(this).closest(".recipe-container").find('li').first().text()
+  console.log(firsIngr)
+})
