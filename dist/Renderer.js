e1ae1abe01fbe7fd7aa1f2bbb584dd0719aa1520
Veronika Adelson <adelson1606@gmail.com>
2020-05-06 19:33:47 +0300

    small fixes

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -2,11 +2,13 @@ class Renderer {
 
   render (data) {
     $('.results').empty()
+    $('.error').empty()
     if (data.length === 0) { //if data is emty arr
       $('.error').append(`
         <div> wrong input! Try another request like: oil, cheese, tomato, etc.</div>
         `) 
     } else {
+
       const source = $('#template').html()
       const template = Handlebars.compile(source)
       const newHTML = template({ data })
